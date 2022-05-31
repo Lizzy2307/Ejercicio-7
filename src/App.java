2c6a75eede98344c356f3b8cceefebfb152a04f6
Lizzy2307 <caceresyani942@gmail.com>
2022-05-30 22:24:21 -0600

    Se agrega el Proyecto

---
+++ src/App.java
@@ -0,0 +1,13 @@
+// Ejercicio 2.23
+//¿Qué imprime el siguiente código?
+
+public class Ejercicio2_23
+{
+    public static void main (String[]args)
+    {
+        System.out.printf(“%s%n%s%n%s%n”, “*”, “***”, “*****”);
+    }
+}
+//*
+//***
+//*****
